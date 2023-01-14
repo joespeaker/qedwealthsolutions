f2e070a1546156e1d19a3faabbc58919335b66a3
joespeaker <joebieker2486@outlook.com>
2023-01-14 13:13:11 -0600

    update tried

--- success.php
+++ success.php
@@ -68,7 +68,78 @@
 </div></a>
 <!-- end of popup-->
 <!-- start of popup-->
-    <?php 
+    <?php
+    
+    use Aws\Ses\SesClient;
+    use Aws\Exception\AwsException;
+    
+    // Create an SesClient. Change the value of the region parameter if you're 
+    // using an AWS Region other than US West (Oregon). Change the value of the
+    // profile parameter if you want to use a profile in your credentials file
+    // other than the default.
+    $SesClient = new SesClient([
+        'profile' => 'default',
+        'version' => '2010-12-01',
+        'region'  => 'us-east-2'
+    ]);
+    
+    // Replace sender@example.com with your "From" address.
+    // This address must be verified with Amazon SES.
+    $sender_email = 'info@qedwealthsolutions.com';
+    
+    // Replace these sample addresses with the addresses of your recipients. If
+    // your account is still in the sandbox, these addresses must be verified.
+    $recipient_emails = 'joebieker2486@outlook.com';
+    
+    // Specify a configuration set. If you do not want to use a configuration
+    // set, comment the following variable, and the
+    // 'ConfigurationSetName' => $configuration_set argument below.
+    $configuration_set = 'ConfigSet';
+    
+    $subject = 'Amazon SES test (AWS SDK for PHP)';
+    $plaintext_body = 'This email was sent with Amazon SES using the AWS SDK for PHP.' ;
+    $html_body =  '<h1>AWS Amazon Simple Email Service Test Email</h1>'.
+                  '<p>This email was sent with <a href="https://aws.amazon.com/ses/">'.
+                  'Amazon SES</a> using the <a href="https://aws.amazon.com/sdk-for-php/">'.
+                  'AWS SDK for PHP</a>.</p>';
+    $char_set = 'UTF-8';
+    
+    try {
+        $result = $SesClient->sendEmail([
+            'Destination' => [
+                'ToAddresses' => $recipient_emails,
+            ],
+            'ReplyToAddresses' => [$sender_email],
+            'Source' => $sender_email,
+            'Message' => [
+              'Body' => [
+                  'Html' => [
+                      'Charset' => $char_set,
+                      'Data' => $html_body,
+                  ],
+                  'Text' => [
+                      'Charset' => $char_set,
+                      'Data' => $plaintext_body,
+                  ],
+              ],
+              'Subject' => [
+                  'Charset' => $char_set,
+                  'Data' => $subject,
+              ],
+            ],
+            // If you aren't using a configuration set, comment or delete the
+            // following line
+            'ConfigurationSetName' => $configuration_set,
+        ]);
+        $messageId = $result['MessageId'];
+        echo("Email sent! Message ID: $messageId"."\n");
+    } catch (AwsException $e) {
+        // output error message if fails
+        echo $e->getMessage();
+        echo("The email was not sent. Error message: ".$e->getAwsErrorMessage()."\n");
+        echo "\n";
+    }
+    
       $q1 = $_POST['q1'];
       $q2 = $_POST['q2'];
       $q3 = $_POST['q3'];
@@ -79,7 +150,6 @@
       $email = $_POST['email'];
 
       $message = "Thanks for taking the quiz, here are your results...";
-      mail($email,"Tax Quiz Results",$message,"From: taxquiz@qedwealthsolutions.com");
       echo "<div class='row top'><div class='col-12 text-center'><p class='emoji'>🎉</p><h1>Awesome " . $fname . "!</h1><h4>You've submitted the quiz. Check your email to see your results!</h4><p>This page will auto-redirect in 10 seconds.</p><p>If auto redirect doesn't work click <a href='index.html'>here</a>!</p></div></div>";
     ?>
     
