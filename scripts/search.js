282b409de6941e573045d8c06d3eac84542983c3
Joe Bieker <joebieker2486@outlook.com>
2023-08-04 15:45:42 -0500

    working filter system

--- scripts/search.js
+++ scripts/search.js
@@ -15,7 +15,7 @@ function search() {
     
 }
 
-function filter() {
+function filter(filterName) {
     //Retirement
     //Tax Planning
     //Money and Kids
@@ -23,6 +23,38 @@ function filter() {
     //Investing
     //Current Events
     //Insights
-    let tags = document.getElementsByTagName('tags');
+    let tags = document.getElementsByClassName('tag');
+    let filterDiv = document.getElementsByClassName('filter');
+    let filter = filterName.toUpperCase();
+
+    for (i=0; i<tags.length; i++) {
+        tags[i].classList.remove('selected');
+        tags[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.classList.remove('safe');
+    }
+
+    //clear selected for all and set selected for clicked filter
+    for (i=0; i<filterDiv.length; i++) {
+        filterDiv[i].classList.remove('selected');
+        if (filterDiv[i].innerText.toUpperCase() == filter) {
+            filterDiv[i].classList.add('selected');
+        }
+    }
+
+    for (i=0; i<tags.length; i++) {
+        let tag = tags[i].innerText.toUpperCase();
+        console.log(tag + ' ' + filter);
+        if (!(tag == filter)) {
+            if(!tags[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.classList.contains('safe')) {
+                tags[i].classList.remove('selected');
+                tags[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.classList.add('hidden');
+            }
+        }
+        else {
+            tags[i].classList.add('selected');
+            tags[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.classList.remove('hidden');
+            tags[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.classList.add('safe');
+        }
+    }
     
 }
+
